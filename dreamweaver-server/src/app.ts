969e99adf1e143ac61335bcd9369001808c0445a
adedamolabasit <adedamolabasit09@gmail.com>
2025-06-09 03:19:23 +0100

    fix deploymenet error

--- dreamweaver-server/src/app.ts
+++ dreamweaver-server/src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: '*',
     allowedHeaders: ["Content-Type", "authorization"],
     credentials: true,
   })
